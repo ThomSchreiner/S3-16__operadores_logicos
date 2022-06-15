c5a2e566f5a1223441381050570039ae8562b4dd
Thomas Schreiner <iteano.thom@hotmail.com>
2022-06-15 15:19:19 -0300

    Ciando arquivos html e js

---
+++ Exercicio_01/script.js
@@ -0,0 +1,7 @@
+let idade = 24
+let avaliarIdade = 0
+if(idade >18 && idade < 25){
+    console.log("A idade está entre 18 e 25")
+}else{
+    console.log("A idade não está entre 18 e 25")
+}
---
+++ Exercicio_02/script.js
@@ -0,0 +1,8 @@
+let carroNome = "Chevrolet"
+let carroAno = 2019 
+let filtrarCarro = (carroNome == "Chevrolet" && carroAno == 2019)
+if(filtrarCarro) {
+    console.log("Carro encontrado")
+}else{
+    console.log("Carro não encontrado")
+}
